7212149800fefc68324bbbca2ee8739fbe4e99b9
Hyuan Peixoto Farrapo <hyuan.farrapo@projeto.lead>
2019-06-03 13:00:59 -0300

    inicio do app

---
+++ hello-world/src/app/test/test.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+
+@NgModule({
+  imports:[
+    FormsModule
+  ]
+})
+export class TestComponent implements OnInit {
+
+  public name = "Teste conteudo dinamico";
+  public filme = "";
+  public values = '';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
