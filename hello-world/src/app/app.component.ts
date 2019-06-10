e797675f068ff812a73d2e3bd5ef5bf5eefb5a62
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2019-06-10 11:58:49 -0300

    uso de mais atributos aria e eventos de teclado para acessibilidade no site

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -9,8 +9,40 @@ export class AppComponent {
   title = 'Catalogo de Filmes';
   public numeroGenero = "";
   public filmeBuscado = "";
+  public descricaoContraste = "Ativar Contraste";
+  public descricaoAumento = "Ativar Aumento de Texto";
+  public descricaoReducao = "Ativar Redução de Texto";
   public opened = false;
   public ativarContraste = false;
   public ativarAumento = false;
   public ativarReducao = false;
+
+  textoContraste(){
+    if(this.ativarContraste){
+      this.descricaoContraste = "Desativar Contraste";
+    }
+    else{
+      this.descricaoContraste = "Ativar Contraste";
+    }
+  }
+
+  textoAumento(){
+    if(this.ativarAumento){
+      this.descricaoAumento = "Desativar Aumento de Texto";
+    }
+    else{
+      this.descricaoAumento = "Ativar Aumento de Texto";
+    }
+  }
+
+  textoReducao(){
+    if(this.ativarReducao){
+      this.descricaoReducao = "Desativar Redução de Texto";
+    }
+    else{
+      this.descricaoReducao = "Ativar Redução de Texto";
+    }
+  }
+
+
 }
